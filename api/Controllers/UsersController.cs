0af776effcec865ad08264a535eafb1e88ac3f8a
Lucas Gomes <lucasggomees@gmail.com>
2023-03-11 17:37:02 +0000

    Set api endpoint in the Users Controller to Async

--- api/Controllers/UsersController.cs
+++ api/Controllers/UsersController.cs
@@ -3,6 +3,7 @@
 using api.Data;
 using api.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -17,28 +18,28 @@ namespace api.Controllers
         /* When this class is created (by EF when the user calls the endpoint ) 
         it also creates an instance of data context called context. 
         This means we will then have available a session with the DB. */
-        public UsersController(DataContext context) 
+        public UsersController(DataContext context)
         {
             _context = context;
 
         }
-        
+
         // ActionResult allows us to return a standard http response (i.e. return BadRequest())
         //This method returns a list of all users
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            var users = _context.Users.ToList();
+            var users = await _context.Users.ToListAsync();
 
             return users;
         }
 
         //This method returns a single user
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
 
-            return _context.Users.Find(id);
+            return await _context.Users.FindAsync(id);
 
         }
     }
