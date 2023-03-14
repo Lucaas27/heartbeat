7713aaae92d70472fda95c5061db1f8cb03f9480
Lucas Gomes <lucasggomees@gmail.com>
2023-03-14 13:04:08 +0000

    feat: Create a register DTO and username check
    Register DTO was created in order to pass the properties from the client to the server in the request body. A method to check if a username already exists in the database was added to the account controller.

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -2,8 +2,10 @@
 using System.Security.Cryptography;
 using System.Text;
 using api.Data;
+using api.DTOs;
 using api.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -18,14 +20,16 @@ namespace api.Controllers
         }
 
         [HttpPost("register")] // POST api/account/register?username=sam&password=password
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is already in user");
+
             using var hmac = new HMACSHA512(); // Hashing algorithm 
 
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
 
             }; //new user object
@@ -34,6 +38,11 @@ namespace api.Controllers
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
